dc8532eac71a2d16c6709d48146b930db007f858
Qixiny <3185835784@qq.com>
2022-01-12 21:29:54 +0800

    feat(scale): object

--- src/object/sprite.ts
+++ src/object/sprite.ts
@@ -1,21 +1,27 @@
 import { NanObject } from "./nanobject";
-import { Transform } from "utils/transform";
+import { Transform } from "../utils/transform";
+import { Vector2 } from "../utils/vector";
+
 /**
  * Sprite类
  * 渲染图片的对象都属于Sprite类，Sprite类属于Nan对象
  */
 export class Sprite extends NanObject {  
   public image: CanvasImageSource; //图像
-
+  public size: Vector2;
   /**
    * 构造函数
    * @param name 对象名称
    * @param transform 变换信息
    * @param image 图像
    */
-  constructor(name: string,transform: Transform,image: CanvasImageSource){
+  constructor(name: string,transform: Transform,image: CanvasImageSource, size?: Vector2){
     super(name,transform);
     this.image = image;
+    if (!size) 
+      this.size = new Vector2(image.width as number,image.height as number);    
+    else
+      this.size = size;
   }
 
   /**
@@ -23,7 +29,13 @@ export class Sprite extends NanObject {
    */
   _update(): void {
     super._update();    
-    this.ctx.drawImage(this.image,this.transform.position.x,this.transform.position.y);
+    this.ctx.drawImage(
+      this.image,
+      this.transform.position.x,
+      this.transform.position.y,
+      this.size.x * this.transform.scale.x,
+      this.size.y * this.transform.scale.y
+    );
   }
 
   /**
--- src/utils/transform.ts
+++ src/utils/transform.ts
@@ -4,13 +4,12 @@ import { Vector2 } from "./vector";
  * 变换信息类中保存的物体的位置和旋转角度等信息
  */
 export class Transform {
-    position:Vector2; //位置
-    rotation:Vector2; //角度 (未实现)
-    
-    constructor(position:Vector2,rotation:Vector2) {
+    position:Vector2 = Vector2.zero; //位置
+    rotation:Vector2 = Vector2.zero; //角度 (未实现)
+    scale:Vector2 = Vector2.zero; //缩放
+    constructor(position: Vector2,rotation: Vector2,scale: Vector2) {
       this.position = position;
       this.rotation = rotation;
-    }
-
-    static zero: Transform = new Transform(new Vector2(0,0),new Vector2(0,0));
+      this.scale = scale;
+    }    
   }
