1d438fca8404eb9ee2712eb625f2531be636573c
liuyang <liuyang@inno-gence.com>
2022-01-19 12:31:19 +0800

    freq   <Body>
      <TestPlan>

--- dist/test.js
+++ dist/test.js
@@ -2,7 +2,7 @@ import {Nan,NText,Transform,Vector,GameObject,NLine, Polygon, Sprite} from './na
 import MapItem from './obj/mapitem.js'
 import MapManager from './manager/mapmanager.js'
 
-let nan = new Nan(document.getElementById('canvas'),1);
+let nan = new Nan(document.getElementById('canvas'),6);
 nan.canvasDraggable = true;
 nan.extraCleanRect = new Vector(600,600);
 let mapManager = new MapManager(99,99,new Vector(5,5));
